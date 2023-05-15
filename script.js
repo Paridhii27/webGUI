dafcb6dd7fd0734de37a3c7117b45ddb802c34a4
Paridhi <paridhigarg27@gmail.com>
2023-05-15 00:09:05 -0400

    slider updates

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ let ws = new WebSocket("wss://socket-server-final-project.herokuapp.com/:443");
 //let ws = new WebSocket("ws://localhost:5001");
 let controlledbyTD = document.querySelector (".controlledbyTD")
 let controlTD = document.querySelector(".controlTD")
-console.log(controlTD.value)
+
+let controlledbyTD2 = document.querySelector (".controlledbyTD2")
+let controlTD2 = document.querySelector(".controlTD2")
 // values in td go from 0 to 1 and in web it goes from 0 to 100 
 
 controlTD.addEventListener ('input', (event) =>{
-  console.log(controlTD.value)
+  console.log("slider1",controlTD.value)
   ws.send( JSON.stringify({"slider1" : controlTD.value/ 100.0}))
+})
+
+controlTD2.addEventListener ('input', (event) =>{
+  console.log("slider2",controlTD2.value)
+  ws.send( JSON.stringify({"slider2" : controlTD2.value/ 100.0}))
 })
 
 // change for only the final changed value 
@@ -17,18 +24,24 @@ ws.addEventListener('open', (event) => {
 });
 
 ws.addEventListener('message', (message) => {
-  if (message.data == "ping" ){
-    ws.send ("pong")
+  if (message.data == 'ping' ){
+    ws.send ('pong')
     return
   }
 
   let data = JSON.parse(message.data);
-  if ("slider1" in data) { 
-    let val = data["slider1"];
+  if ('slider1' in data) {
+    let val = data['slider1']
     controlledbyTD.value = val * 100;
-    console.log('val', val)
+    console.log('val', val);
   }
-  console.log(message);
+
+  else if ('slider2' in data) {
+    let val2 = data['slider2']
+    controlledbyTD2.value = val2 * 100;
+    console.log('val', val2);
+  }
+  console.log(data);
 });
 
 ws.addEventListener('error', (error) => {
